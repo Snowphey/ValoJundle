d49b754234f15d223ea2ed31d8139533feabcaf2
Lucas Ransan <lucas@ransan.fr>
2025-05-29 02:42:14 +0200

    close server on sigterm

--- backend.js
+++ backend.js
@@ -505,6 +505,13 @@ cron.schedule('0 0 * * *', async () => {
 
 app.use(express.static('dist'));
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`ValoJundle backend running on port ${PORT}`);
+});
+
+process.on('SIGTERM', () => {
+  debug('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    debug('HTTP server closed');
+  });
 });
